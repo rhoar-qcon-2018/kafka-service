1a2545467f84128bb84ed966de993d131f21064f
Deven Phillips <deven.phillips@redhat.com>
2018-06-25 01:44:50 -0400

    Revert "Send second message after delay"
    This reverts commit 9c72d508de8066cbbfa8f88fc0c87e40a446c317.

--- src/test/groovy/com/redhat/qcon/kafka/MainVerticleSpec.groovy
+++ src/test/groovy/com/redhat/qcon/kafka/MainVerticleSpec.groovy
@@ -53,10 +53,10 @@ class MainVerticleSpec extends Specification {
                     println('Completed publish of message')
                 })
 
-                vertx.setTimer(5000, { t ->
-                    KafkaService.createProxy(vertx, 'kafka.service').publish(message, { res1 ->
-                        println('Completed publish of message')
-                    })
+                Thread.sleep(5000)
+
+                KafkaService.createProxy(vertx, 'kafka.service').publish(message, { res1 ->
+                    println('Completed publish of message')
                 })
             })
         then: 'Expect a successful deployment and successful message exchange'
